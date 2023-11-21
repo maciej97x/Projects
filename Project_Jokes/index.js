8ae0bdf0c512e935780ccfce168193fa2a23bed8
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-21 09:44:46 +0000

    commit

---
+++ Project_Jokes/index.js
@@ -0,0 +1,38 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "Hlhx9vJWZY6c8xv0wwSy1Q==SF5gtaeWkZHzdafM";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+
+async function getJoke() {
+
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+    
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+    
+        jokeEl.innerText = data[0].joke;
+        
+    } catch (error) {
+      jokeEl.innerText = "An error happend, try again later";
+      btnEl.disabled = false;
+      btnEl.innerText = "Tell me a joke";
+      console.log(error);
+    }
+}
+
+btnEl.addEventListener("click", getJoke);
