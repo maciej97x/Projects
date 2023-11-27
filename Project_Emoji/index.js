11ad2f5be7deaeab21cc8ba19b57e4859a765aa6
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-27 10:32:24 +0000

    commit

---
+++ Project_Emoji/index.js
@@ -0,0 +1,26 @@
+const btnEl = document.getElementById("btn");
+const emojiNameEl = document.getElementById("emoji-name");
+
+const emoji = [];
+
+async function getEmoji(){
+    let response = await fetch("https://emoji-api.com/emojis?access_key=07a508be72606c1e0a2f422e3af98199a612c5cd")
+
+    data = await response.json()
+
+    for (let i = 0; i < 1500; i++){
+        emoji.push({
+            emojiName: data[i].character,
+            emojiCode: data[i].unicodeName,
+        });
+    }
+}
+
+getEmoji();
+
+
+btnEl.addEventListener("click", () => {
+    const randomNum = Math.floor(Math.random() * emoji.length)
+    btnEl.innerText = emoji[randomNum].emojiName;
+    emojiNameEl.innerText = emoji[randomNum].emojiCode;
+});
