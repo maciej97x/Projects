d9bfbb745b7efa1dc146eaeca692be42b39b14a5
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-29 13:26:08 +0000

    commit

--- Project_Images/index.js
+++ Project_Images/index.js
@@ -3,7 +3,7 @@ const accessKey = "-jaU3x9bu3065DZhmEEDfGXZUsWdnIXFt2YngpFL1Dk"
 const formEl = document.querySelector("form")
 const searchInputEl = document.getElementById("search-input")
 const searchResultsEl = document.querySelector(".search-results")
-const showMoreButton = document.getElementById("show-more-button")
+const showMoreButtonEl = document.getElementById("show-more-button")
 
 let inputData = "";
 let page = 1;
@@ -11,7 +11,6 @@ let page = 1;
 async function searchImages(){
     inputData = searchInputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
     if (page === 1) {
@@ -20,11 +19,36 @@ async function searchImages(){
 
     const results = data.results;
 
+    results.map((result)=>{
+        const imageWrapper = document.createElement("div")
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a")
+        imageLink.href = result.links.innerHTML
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResultsEl.appendChild(imageWrapper);
+    })
+    page++;
+
+    if(page > 1){
+        showMoreButtonEl.style.display = "block";
+    }
+
     console.log(results);
 }
 
 formEl.addEventListener("submit", (event)=>{
     event.preventDefault();
     page = 1;
+    searchImages();
+});
+
+showMoreButtonEl.addEventListener("click", ()=>{
     searchImages();
 });
