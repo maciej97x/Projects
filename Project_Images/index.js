1c2d1b5dc942b63fa20dad76cadf6150e7509cd2
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-29 12:30:12 +0000

    commit

---
+++ Project_Images/index.js
@@ -0,0 +1,8 @@
+const accessKey = "-jaU3x9bu3065DZhmEEDfGXZUsWdnIXFt2YngpFL1Dk"
+
+const formEl = document.querySelector("form")
+const searchInputEl = document.getElementById("search-input")
+const searchResultsEl = document.querySelector("search-input")
+const showMoreButton = document.getElementById("show-more-button")
+
+
